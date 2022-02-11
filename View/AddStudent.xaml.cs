4cb63f38e995548be1cc44d2ba2d73833a4faa14
daolingluo <2321442270@qq.com>
2022-02-11 23:52:56 +0800

    添加项目文件。

---
+++ View/AddStudent.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Demo2022.Model;
+using Demo2022.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace Demo2022.View
+{
+    // <summary>
+    /// AddStudent.xaml 的交互逻辑
+    /// </summary>
+    public partial class AddStudent : Window
+    {
+        private ViewModel.Demo2022ViewModel viewModel;
+        public AddStudent(ObservableCollection<StudentInfo> StudentList)
+        {
+            InitializeComponent();
+            this.DataContext = new Demo2022ViewModel();
+            //刷新表格
+            viewModel = DataContext as ViewModel.Demo2022ViewModel;
+            this.viewModel.StudentList = StudentList;
+        }
+    }
+}
---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Demo2022.Model;
+using Demo2022.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo2022
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new Demo2022ViewModel();
+        }
+        /// <summary>
+        /// 每增加一行，自动+1
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void dataGrid_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            e.Row.Header = e.Row.GetIndex() + 1;
+        }
+    }
+}
