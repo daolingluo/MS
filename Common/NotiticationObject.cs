df212e36800c23d36b5da574b27a21a9361a42a9
daolingluo <2321442270@qq.com>
2022-02-11 23:52:56 +0800

    添加项目文件。

---
+++ Common/NotiticationObject.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo.Common
+{
+    public class NotiticationObject : INotifyPropertyChanged, IDataErrorInfo
+    {
+        #region 数据验证
+        public string this[string columnName]
+        {
+            get
+            {
+                var vc = new ValidationContext(this, null, null);
+                vc.MemberName = columnName;
+                var res = new List<ValidationResult>();
+                var result = Validator.TryValidateProperty(this.GetType().
+                    GetProperty(columnName).GetValue(this, null), vc, res);
+                if (res.Count > 0)
+                {
+                    return string.Join(Environment.NewLine, res.Select(r => r.ErrorMessage).ToArray());
+                }
+                return string.Empty;
+            }
+        }
+
+        public string Error
+        {
+            get { return ""; }
+        }
+
+        /// <summary>
+        /// 页面中是否所有控制数据验证正确
+        /// </summary>
+        public virtual bool IsValid { get; set; }
+
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// 发起通知
+        /// </summary>
+        /// <param name="propertyName">属性名称</param>
+        public void RaisePropertyChanged(string propertyName)
+        {
+            //在属性改变不等于空的情况下才来监听
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Model/StudentInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo2022.Model
+{
+    public class StudentInfo
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+
+
+        public string TeamName { get; set; }
+    }
+}
