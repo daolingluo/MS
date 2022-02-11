0086597a5f1232fd06b983f7e45a292cbb6e9010
daolingluo <2321442270@qq.com>
2022-02-11 23:52:56 +0800

    添加项目文件。

---
+++ Common/FormatUtil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo.Common
+{
+    //这是类型参数约束，where表名了对类型变量T的约束关系。
+    //where T：FormatUtil表示类型变量是继承于FormatUtil的，
+    //或者是FormatUtil本身。where T: new()指明了创建T的实例应该使用的构造函数。
+    //.NET支持的类型参数约束
+    //where T: new()  T必须要有一个无参构造函数
+    /// <summary>
+    /// 工具类，转化
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class FormatUtil<T> where T : class, new()
+    {
+        public static ObservableCollection<T> GetObservableCollection(List<T> tList)
+        {
+            return new ObservableCollection<T>(tList);
+        }
+    }
+}
---
+++ Model/TeamInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo2022.Model
+{
+    public class TeamInfo
+    {
+        [Key]
+        public int Id { get; set; }
+        public string TeamName { get; set; }
+    }
+}
